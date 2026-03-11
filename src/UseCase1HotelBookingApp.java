63a344cfa8e8b7d1e7a3bd7fcb3abc94e9078741
praj1n <ps4006@srmist.edu.in>
2026-03-11 14:58:48 +0530

    Initial commit - BookMyStay project

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,24 @@
+/**
+ * Book My Stay Application
+ * This class represents the entry point of the Hotel Booking System.
+ * It prints the welcome message and application version.
+ *
+ * @author praj1n
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Main method - program execution starts here
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
+        System.out.println("Application Started Successfully!");
+    }
+}
